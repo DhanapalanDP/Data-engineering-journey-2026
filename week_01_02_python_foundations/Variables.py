9666870e17552a62d8747777c101e037a265d2a8
DhanapalanDP <78578296+DhanapalanDP@users.noreply.github.com>
2026-02-17 13:12:27 +0530

    Update Variables.py

--- week_01_02_python_foundations/Variables.py
+++ week_01_02_python_foundations/Variables.py
@@ -1,4 +1,4 @@
-# Create the variables monthly_savings and num_months
+1. # Create the variables monthly_savings and num_months
 monthly_savings = 10
 num_months = 4
 
@@ -7,3 +7,16 @@ new_savings = monthly_savings * num_months
 
 # Print new_savings
 print(new_savings)
+
+2. # Create a variable half
+
+half = 0.5
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+
+print(half) 
+print(intro)
+print(is_good)
