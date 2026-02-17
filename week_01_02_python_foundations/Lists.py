629afa9b8d8adb9377271b49b0915ab7cd353533
DhanapalanDP <78578296+DhanapalanDP@users.noreply.github.com>
2026-02-17 13:53:56 +0530

    Create Lists.py

---
+++ week_01_02_python_foundations/Lists.py
@@ -0,0 +1,15 @@
+my_favorite_foods = ["Pizza", "Dosa", "Ice Cream"]
+
+print("My absolute favorite food is:", my_favorite_foods[1])
+
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall,kit,liv,bed,bath]
+
+# Print areas
+print("Areas:", areas)
