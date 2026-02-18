442c2ffe29470841df3c0585dedbe6bf77680470
DhanapalanDP <78578296+DhanapalanDP@users.noreply.github.com>
2026-02-18 15:14:06 +0530

    Add files via upload

---
+++ week_01_02_python_foundations/Packages.py
@@ -0,0 +1,16 @@
+# Calling the library for the math tools!
+import math
+
+# Now we can use special math tools, like finding the square root of a number!
+my_number = math.pow(5, 2)
+
+print(my_number)
+# The screen prints: 5.0
+
+import numpy as np
+
+x = np.array([1, 2, 3, 4, 5])
+# The screen prints: array([1, 2, 3, 4, 5])
+
+print(x)
+
---
+++ week_01_02_python_foundations/Variables1.py
@@ -0,0 +1,8 @@
+# My first local Python script!
+my_age = 25
+my_name = "Dhanapalan"
+is_learning = True
+
+print("My name is:", my_name)
+print("My Age is:", my_age)
+print("Am I learning Data Engineering?", is_learning)
